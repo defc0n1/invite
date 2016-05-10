569a814bdbbe50c07494988b5b83049a3c24e4dc
Mohit Aghera <mhaghera_alpha@yahoo.co.in>
2016-05-10 23:20:06 +0530

    Implement withdraw and resent invite access controller, so user can only withdraw or resend own invitations.

---
+++ src/Controller/InviteAccessController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\invite\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines the access control handler for invite routes.
+ */
+class InviteAccessController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function withdrawInviteAccess(AccountInterface $account) {
+    $badge_admin = $account->hasPermission('administer invite settings');
+
+    if ($badge_admin) {
+      return AccessResult::allowed();
+    }
+    else {
+      /** @var \Drupal\invite\InviteInterface $invite */
+      $invite_from_url = \Drupal::routeMatch()->getParameter('invite');
+      return AccessResult::allowedIf($account->id() && $account->id() == $invite_from_url->getOwnerId() && $account->hasPermission('withdraw own invitations'))
+        ->cachePerPermissions()
+        ->cachePerUser();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resendInviteAccess(AccountInterface $account) {
+    $badge_admin = $account->hasPermission('administer invite settings');
+
+    if ($badge_admin) {
+      return AccessResult::allowed();
+    }
+    else {
+      /** @var \Drupal\invite\InviteInterface $invite */
+      $invite_from_url = \Drupal::routeMatch()->getParameter('invite');
+      return AccessResult::allowedIf($account->id() && $account->id() == $invite_from_url->getOwnerId())
+        ->cachePerPermissions()
+        ->cachePerUser();
+    }
+  }
+}
